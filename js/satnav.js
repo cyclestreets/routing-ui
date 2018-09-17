252e25633180ee83a3684bfca306ec4b37fe7c7c
mvl22 <martin@lucas-smith.co.uk>
2018-09-17 02:20:50 +0100

    Add buildings

--- js/satnav.js
+++ js/satnav.js
@@ -53,6 +53,9 @@ var satnav = (function ($) {
 			// Add layer switching
 			satnav.layerSwitcher ();
 			
+			// Add buildings
+			satnav.addBuildings ();
+			
 			// Add routing
 			satnav.routing ();
 		},
@@ -209,6 +212,51 @@ var satnav = (function ($) {
 			_map.on ('style.load', function () {
 				_map.addSource (route.layer.source, route.source);
 				_map.addLayer (route.layer);
+			});
+		},
+		
+		
+		// Buildings layer
+		// https://www.mapbox.com/mapbox-gl-js/example/3d-buildings/
+		addBuildings: function ()
+		{
+			// The 'building' layer in the mapbox-streets vector source contains building-height data from OpenStreetMap.
+			_map.on('style.load', function() {
+				// Insert the layer beneath any symbol layer.
+				var layers = _map.getStyle().layers;
+				
+				var labelLayerId;
+				for (var i = 0; i < layers.length; i++) {
+					if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+						labelLayerId = layers[i].id;
+						break;
+					}
+				}
+				
+				_map.addLayer ({
+					'id': '3d-buildings',
+					'source': 'composite',
+					'source-layer': 'building',
+					'filter': ['==', 'extrude', 'true'],
+					'type': 'fill-extrusion',
+					'minzoom': 15,
+					'paint': {
+						'fill-extrusion-color': '#aaa',
+						
+						// Use an 'interpolate' expression to add a smooth transition effect to the buildings as the user zooms in
+						'fill-extrusion-height': [
+							"interpolate", ["linear"], ["zoom"],
+							15, 0,
+							15.05, ["get", "height"]
+						],
+						'fill-extrusion-base': [
+							"interpolate", ["linear"], ["zoom"],
+							15, 0,
+							15.05, ["get", "min_height"]
+						],
+						'fill-extrusion-opacity': .6
+					}
+				}, labelLayerId);
 			});
 		}
 	};
