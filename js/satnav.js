9ccdad3e29bbc7d9d8974384e81fde5fbff26b93
mvl22 <martin@lucas-smith.co.uk>
2020-07-04 18:38:13 +0100

    Remove pitch gesture handling events as this is now handled nativelyin Mapbox GL JS v.1.10.0
    https://github.com/mapbox/mapbox-gl-js/releases/tag/v1.10.0

--- js/satnav.js
+++ js/satnav.js
@@ -117,9 +117,6 @@ var satnav = (function ($) {
 			// Enable tilt and direction
 			satnav.enableTilt ();
 			
-			// Enable pitch gestures
-			satnav.enablePitchGestures ();
-			
 			// Add buildings
 			satnav.addBuildings ();
 			
@@ -289,51 +286,6 @@ var satnav = (function ($) {
 		setPanningEnabled: /* public */ function (panningEnabled)
 		{
 			_panningEnabled = panningEnabled;
-		},
-		
-		
-		// Enable pitch gesture handling
-		// See: https://github.com/mapbox/mapbox-gl-js/issues/3405#issuecomment-449059564
-		enablePitchGestures: function ()
-		{
-			// Two-finger gesture on mobile for pitch; see: https://github.com/mapbox/mapbox-gl-js/issues/3405#issuecomment-449059564
-			_map.on ('touchstart', function (data) {
-				if (data.points.length == 2) {
-					var diff = Math.abs(data.points[0].y - data.points[1].y);
-					if (diff <= 50) {
-						data.originalEvent.preventDefault();	//prevent browser refresh on pull down
-						_map.touchZoomRotate.disable();	 //disable native touch controls
-						_map.dragPan.disable();
-						self.dpPoint = data.point;
-						self.dpPitch = _map.getPitch();
-					}
-				}
-			});
-			
-			_map.on ('touchmove', function (data) {
-				if (self.dpPoint) {
-					data.preventDefault();
-					data.originalEvent.preventDefault();
-					var diff = (self.dpPoint.y - data.point.y) * 0.5;
-					_map.setPitch(self.dpPitch + diff);
-				}
-			});
-			
-			_map.on ('touchend', function (data) {
-				 if (self.dpPoint) {
-					_map.touchZoomRotate.enable();
-					_map.dragPan.enable();
-				}
-				self.dpPoint = null;
-			});
-			
-			_map.on ('touchcancel', function (data) {
-				if (self.dpPoint) {
-					_map.touchZoomRotate.enable();
-					_map.dragPan.enable();
-				}
-				self.dpPoint = null;
-			});
 		},
 		
 		
