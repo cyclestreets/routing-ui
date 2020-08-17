077f1f315bb914fbe6bf58ebcee9b6e10c4de8ad
Siequnu <11821606+Siequnu@users.noreply.github.com>
2020-08-17 10:12:09 +0800

    Remove ES6 syntax

--- js/routing.js
+++ js/routing.js
@@ -1586,20 +1586,20 @@ var routing = (function ($) {
 		// Throttler function
 		throttle: function (func, limit) 
 		{
-			let lastFunc
-			let lastRan
+			var lastFunc;
+			var lastRan;
 			return function () {
-				const context = this
-				const args = arguments
+				const context = this;
+				const args = arguments;
 				if (!lastRan) {
-					func.apply(context, args)
-					lastRan = Date.now()
+					func.apply(context, args);
+					lastRan = Date.now();
 				} else {
 					clearTimeout(lastFunc)
 					lastFunc = setTimeout(function () {
 						if ((Date.now() - lastRan) >= limit) {
-							func.apply(context, args)
-							lastRan = Date.now()
+							func.apply(context, args);
+							lastRan = Date.now();
 						}
 					}, limit - (Date.now() - lastRan))
 				}
