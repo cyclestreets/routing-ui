706dd3e58aeec5b112cd48c540f41bbc9e599206
mvl22 <martin@lucas-smith.co.uk>
2022-09-25 21:24:09 +0100

    Add GPX link to journey results card - image size

--- js/routing.js
+++ js/routing.js
@@ -1687,7 +1687,7 @@ var routing = (function ($) {
 			html += '<li><img src="/images/icon-clock.svg" alt="Clock icon" /><p> ' + timeFormatted + 'in</p></li>';
 			html += '<li><img src="/images/icon-flame.svg" alt="Flame icon" /><p> ' + geojson.properties.plans[strategy.id].kiloCaloriesBurned + ' calories</p></li>';
 			html += '<li><img src="/images/icon-leaf.svg" alt="Leaf icon" /><p> ' + geojson.properties.plans[strategy.id].grammesCO2saved + ' g</p></li>';
-			html += '<li><img src="/images/icon-jp-red.svg" alt="" /><p><a href="' + gpxLink + '">GPX</a></p></li>';
+			html += '<li><img src="/images/icon-jp-red.svg" alt="" width="12" height="12" /><p><a href="' + gpxLink + '">GPX</a></p></li>';
 			html += '</ul>';
 
 			html += '<div class="elevation-chart-container"><canvas id="' + strategy.id + 'elevationChart"></canvas></div>';
