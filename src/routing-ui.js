9360c1c1e6cbe5f17277ffd85095b7e0bc044441
mvl22 <martin@lucas-smith.co.uk>
2023-04-14 12:42:03 +0100

    In toolbox, convert nested code to guard clause

--- src/routing-ui.js
+++ src/routing-ui.js
@@ -405,27 +405,28 @@ var routing = (function ($) {
 		// Function to add a toolbox
 		toolbox: function ()
 		{
-			if (_settings.showToolBox) {
-				// Add layer switcher UI
-				var control = routing.createControl ('toolbox', 'bottom-left');
-				
-				// Determine whether route loading from ID is supported; for this, all engines need to be native CycleStreets type
-				var routeLoadingSupported = true;
-				$.each (_settings.strategies, function (index, strategy) {
-					if (strategy.routeRequest) {
-						routeLoadingSupported = false;
-					}
-				});
-				
-				// Construct HTML for layer switcher
-				var html = '<ul id="toolbox">';
-				if (routeLoadingSupported) {
-					html += '<li><a id="loadrouteid" href="#">Load route ID &hellip;</a></li>';
+			// End if not required
+			if (!_settings.showToolBox) {return;}
+			
+			// Add layer switcher UI
+			var control = routing.createControl ('toolbox', 'bottom-left');
+			
+			// Determine whether route loading from ID is supported; for this, all engines need to be native CycleStreets type
+			var routeLoadingSupported = true;
+			$.each (_settings.strategies, function (index, strategy) {
+				if (strategy.routeRequest) {
+					routeLoadingSupported = false;
 				}
-				html += '<li><a id="panning" href="#" class="hidden">Panning: disabled</a></li>';
-				html += '</ul>';
-				$('#toolbox').append (html);
+			});
+			
+			// Construct HTML for layer switcher
+			var html = '<ul id="toolbox">';
+			if (routeLoadingSupported) {
+				html += '<li><a id="loadrouteid" href="#">Load route ID &hellip;</a></li>';
 			}
+			html += '<li><a id="panning" href="#" class="hidden">Panning: disabled</a></li>';
+			html += '</ul>';
+			$('#toolbox').append (html);
 		},
 		
 		
